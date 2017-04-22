abcfc2e77e12871b30ba5916728d480068a0d158
Sameh Mabrouk <smabrouk@mobiquityinc.com>
2017-04-22 14:50:51 +0200

    Initial Commit

---
+++ Takeaway/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Takeaway
+//
+//  Created by Sameh Mabrouk on 22/04/2017.
+//  Copyright © 2017 Sameh Mabrouk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
